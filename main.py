bd30906a44db1673c9ab4a21e57ecb3d507e2952
nadahkhaledd <nadahkhaledd@gmail.com>
2022-01-13 15:17:51 +0200

    made REST API Methods

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from config import app
+from dotenv import load_dotenv
+from components.models import db
+import os
 
+load_dotenv()
+port = os.getenv('PORT')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(debug=True, port=port)
 
